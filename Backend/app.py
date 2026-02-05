0afd16c8741b0d3818bc685c7842db5632f9e9c4
user.name : "Harsh Nayyar <omharshvnayyar7@gmail.com>
2026-02-05 20:16:33 +0530

    * added

--- Backend/app.py
+++ Backend/app.py
@@ -18,7 +18,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[ "http://localhost:3000",  
-        "ethara-ai-s48k.vercel.app"],
+        "ethara-ai-s48k.vercel.app","*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
