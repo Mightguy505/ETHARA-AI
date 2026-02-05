80d0ea7a29a59b2b7a6abbc0f19c0f9b11b9955b
user.name : "Harsh Nayyar <omharshvnayyar7@gmail.com>
2026-02-05 19:52:11 +0530

    New commmit

--- Backend/app.py
+++ Backend/app.py
@@ -18,7 +18,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[ "http://localhost:3000",  
-        "https://your-frontend.vercel.app","*"],
+        "https://your-frontend.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
